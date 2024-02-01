4ba26bfb8dbe166de5fbf3fe22a3763370ecbc9a
Nicolasalan <nicolasalangrotti03@gmail.com>
2024-01-31 23:23:18 -0300

    finished topic move fast with FastAPI

---
+++ src/fastapi/fast/README.md
@@ -0,0 +1,15 @@
+#### Para executar o docker compose
+```bash
+docker-compose up --build
+```
+#### Não esquecer de dar permissão:
+```bash
+chmod 777 *
+```
+
+* `Pydantic`: É usado para analisar e validar dados
+* `1xx`: information response
+* `2xx`: sucesss
+* `3xx`: redirection
+* `4xx`: Client error
+* `5xx`: Server error
--- src/fastapi/fast/book.py
+++ src/fastapi/fast/book.py
@@ -6,13 +6,15 @@ from starlette import status
 
 app = FastAPI()
 
+# validation data type
 class Book:
+    # utiliza o Field para fazer validações como min_length, max_length, gt, lt
     id: int
     title: str
-    author: str
-    description: str
-    rating: int
-    published_date: int
+    author: str 
+    description: str 
+    rating: int 
+    published_date: int 
 
     def __init__(self, id, title, author, description, rating, published_date):
         self.id = id
@@ -28,8 +30,10 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)
-    published_date: int = Field(gt=1999, lt=2031) 
+    published_date: int = Field(gt=1999, lt=2040) 
 
+
+    # Mostrar como colocar os dados chamado de Swagger Pydantic Config
     class Config:
         schema_extra = {
             'example': {
@@ -49,12 +53,70 @@ BOOKS = [
     Book(5, 'HP2', 'Author 2', 'Book Description', 3, 2027),
     Book(6, 'HP3', 'Author 3', 'Book Description', 1, 2026)
 ]
+# create hello
+@app.get("/")
+async def root():
+    return {"message": "Funcionando"}
 
 @app.get("/books", status_code=status.HTTP_200_OK)
 async def read_all_books():
     return BOOKS
 
+# fetch a book by id
+@app.get("/books/{book_id}", status_code=status.HTTP_200_OK)
+async def read_book(book_id: int = Path(..., gt=0)):# caso escolha um id menor que 0, ele retorna um erro
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+    raise HTTPException(status_code=404, detail='Item não encontrado')
+
+# fetch a book by rating
+@app.get("/books/")
+async def read_book_by_rating(rating: int = Query(None, gt=0, lt=6)):
+    books_return = []
+    for book in BOOKS:
+        if book.rating == rating:
+            books_return.append(book)
+    return books_return
+
+
 @app.post("/create-book", status_code=status.HTTP_201_CREATED)
 async def create_book(book_request: BookRequest):
-    new_book = Book(**book_request.dict())
+    new_book = Book(**book_request.dict()) # o dict() é para converter o objeto em um dicionário
     BOOKS.append(find_book_id(new_book))
+
+    return "Book created successfully"
+
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    return book
+
+# enhance the book
+@app.put("/books/update_book", status_code=status.HTTP_204_NO_CONTENT)
+async def update_book(book: BookRequest):
+    book_changed = False
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+            book_changed = True
+    if not book_changed:
+        raise HTTPException(status_code=404, detail='Item not found')
+
+@app.delete("/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id: int = Path(..., gt=0)):
+    book_changed = False
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            book_changed = True
+            break
+    if not book_changed:
+        raise HTTPException(status_code=404, detail='Item not found')
+
+@app.get("/books/publish/", status_code=status.HTTP_200_OK)
+async def get_book_by_published_date(published_date: int):
+    books_return = []
+    for book in BOOKS:
+        if book.published_date == published_date:
+            books_return.append(book)
+    return books_return
